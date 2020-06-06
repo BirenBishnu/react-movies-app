43f6af267690859a8238395404c0a7ce6f3a3f4e
BirenBishnu <birenbishnu@gmail.com>
2020-06-06 06:40:11 +0530

    Added Home component

---
+++ src/screens/home/home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './home.css';
+
+class home extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+
+}
+export default home;
