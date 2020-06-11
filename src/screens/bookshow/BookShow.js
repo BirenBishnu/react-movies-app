6efcecfcb4eb36936a8409b75b22f811597df920
BirenBishnu <birenbishnu@gmail.com>
2020-06-12 01:33:39 +0530

    Added Confirmation component for showing confirmation page for booking

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import Home from "../../screens/home/Home";
+import Confirmation from '../confirmation/Confirmation';
 import './BookShow.css';
 import Typography from '@material-ui/core/Typography';
 import language from '../../common/language';
@@ -69,6 +70,7 @@ class BookShow extends Component{
         this.state.showDate === "" ? this.setState({showDateRequired: "dispBlock"}) : this.setState({showDateRequired: "dispNone"} );
         this.state.showTime === "" ? this.setState({showTimeRequired: "dispBlock"}) : this.setState({showTimeRequired: "dispNone"} );
         this.state.tickets === "" ? this.setState({ticketsRequired: "dispBlock"}) : this.setState({ticketsRequired: "dispNone"} );
+        ReactDOM.render(<Confirmation bookingSummary={this.state} />, document.getElementById('root'));
     }
 
 
